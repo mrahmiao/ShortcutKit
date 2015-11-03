da4489707e250af52a1959cf44a3c9856652ffae
mrahmiao <mrahmiao@gmail.com>
2015-11-03 10:30:03 +0800

    Initial Commit

---
+++ ShortcutKit/ShortcutKit.h
@@ -0,0 +1,19 @@
+//
+//  ShortcutKit.h
+//  ShortcutKit
+//
+//  Created by mrahmiao on 11/3/15.
+//  Copyright © 2015 Code4Blues. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ShortcutKit.
+FOUNDATION_EXPORT double ShortcutKitVersionNumber;
+
+//! Project version string for ShortcutKit.
+FOUNDATION_EXPORT const unsigned char ShortcutKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShortcutKit/PublicHeader.h>
+
+
